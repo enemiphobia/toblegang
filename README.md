30bcca8d17dc1b46017977183f53dd36c01b55cd
shainevferns <112573839+shainevferns@users.noreply.github.com>
2024-08-29 00:52:56 +0000

    hof revamp + music player and added css stuff

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ it's on my school account because i can't get visual studio code to sign out
 eh all my projects are on here so i might as well use this
 
 it's just a **hobby related thing** tho nothing *school related*
+
+Notes for me --------------------------------------------------------------
+
+`Goals:
+
+* add everyone i need to hall of fame
+* implement mobile-support
+* redo the footer (socials bar looks wack)
+`
+
+To add someone to the hall of fame:
+* just add a <p> element and type something in it
+    (make sure it's within the "awesomePPL" div and the "box" div")
---
+++ public/scripts/sound.js
@@ -0,0 +1,32 @@
+window.player = document.getElementById('player');
+var playpause = document.getElementById('playpause');
+var songtitle = document.getElementById('songtitle');
+changesongtitle();
+player.volume = 0.025; // suuuuper low
+player.play(); // autoplay
+
+playpause.onclick = function () {
+    
+    if (player.paused) {
+		changesongtitle();
+        player.play();
+        playpause.innerHTML = 'pause';
+    } else {
+        player.pause();
+        playpause.innerHTML = 'play';
+        changesongtitle();
+    }
+}
+
+
+
+function changesongtitle() {
+
+	var songtitle = document.getElementById('songtitle');
+    
+	if(player.src = "/public/sounds/ANOTHER HIM.mp3")
+    {
+        songtitle.innerHTML = "Song: ANOTHER HIM - Toby Fox";
+    }
+
+}

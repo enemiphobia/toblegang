50fb075161dee19a02fe395ecc5316a69e3a6713
shainevferns <112573839+shainevferns@users.noreply.github.com>
2024-08-30 18:37:20 +0000

    omori font don't work but the rest of em do so now i'm not sure how to go about this

---
+++ scripts/swearing.js
@@ -0,0 +1,11 @@
+// !!1 = true;
+// !!0 = false;
+
+let test = !!parseInt(req.query.test);
+
+let swear = document.getElementById("swear");
+
+// remove swears
+window.onload = function () {
+    
+}

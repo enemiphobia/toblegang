4d1a871602f15740308a2227c935d1d5cb55a7ae
shainevferns <112573839+shainevferns@users.noreply.github.com>
2024-08-30 04:11:52 +0000

    fixed localhost directory

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,4 +1,10 @@
 window.transitionToPage = function(href) {
+
+    if (window.location.origin = "http://127.0.0.1:5500/") {
+        href = window.location.origin + href.substring(10);
+    }
+    
+
     document.querySelector('body').style.opacity = 0
     setTimeout(function() { 
         window.location.href = href
